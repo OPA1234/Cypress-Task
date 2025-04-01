1219dad1d2b62bf384a83aa7a22e0a60d8b129ab
Nikola_Lukovic <83911165+OPA1234@users.noreply.github.com>
2025-04-01 23:13:31 +0200

    Added delay

--- cypress/e2e/CypressTask/01CreateTicket.cy.js
+++ cypress/e2e/CypressTask/01CreateTicket.cy.js
@@ -8,7 +8,7 @@ describe('Ticket Management Tests', () => {
 
     it('Create Ticket', () => {
         cy.get('i.anticon.text-xl.text-white.anticon-plus svg').click(); // Click on the + sign from left menu
-        cy.get('input[formcontrolname="title"]').type(ticketTitle, { delay: 100 }); // Enter Short summery 
+        cy.get('input[formcontrolname="title"]').type(ticketTitle, { delay: 200 }); // Enter Short summery 
         cy.get('.ql-container.ql-snow').type(ticketDescription); //Enter Description
         cy.get("button[type='submit'] span").click(); // Click on Save button
         cy.contains(ticketTitle).should('be.visible'); // Verfy that ticket is in Backlog
